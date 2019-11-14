7dc5e2fb171c0257637d942f974a6bdcb8ab28db
Alice Kim <alicemeeyoungkim@gmail.com>
2019-11-14 15:26:33 -0800

    add new slide

--- src/imageAPI.js
+++ src/imageAPI.js
@@ -1,10 +1,10 @@
 export const imageAPI = [
-  {
-    id: 0,
-    src: '/john_things_hired.png'
-  },
-  {
-    id: 1,
-    src: '/john_things_hiring.png'
-  }
-]
+	{
+		id: 0,
+		src: '/john_things_hired.png'
+	},
+	{
+		id: 1,
+		src: '/john_things_hiring.png'
+	}
+];
