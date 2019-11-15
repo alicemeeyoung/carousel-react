f5430b8b36a5558aaed7dffab56b89dba7d0a100
Alice Kim <alicemeeyoungkim@gmail.com>
2019-11-14 16:26:53 -0800

    switch to ts

--- src/imageAPI.ts
+++ src/imageAPI.ts
@@ -1,10 +1,10 @@
-export const imageAPI = [
-	{
+export const imageAPI: any = {
+	0: {
 		id: 0,
 		src: '/john_things_hired.png'
 	},
-	{
+	1: {
 		id: 1,
 		src: '/john_things_hiring.png'
 	}
-];
+};
